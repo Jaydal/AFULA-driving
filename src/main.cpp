04cc7b39ce46c33f9ac620572d12d7c40afc8fbf
June Delmar Labitoria <junedelmarlabitoria08@gmail.com>
2024-04-15 12:12:19 +0800

    Add nano env

--- src/main.cpp
+++ src/main.cpp
@@ -3,121 +3,177 @@
 #include <Wire.h>
 #include <Servo.h>
 
+
+int testRunTimer = 10;
+int timer = 0;
+
 AF_DCMotor MOTOR_3(3, MOTOR12_64KHZ);
 AF_DCMotor MOTOR_4(4, MOTOR12_64KHZ);
+AF_DCMotor LED_1(1, MOTOR12_64KHZ);
+AF_DCMotor LED_2(2, MOTOR12_64KHZ);
 
-int EXT_SERVO = 9;
-Servo servo;
-int angle = 0; 
+int EXT_SERVO1 = 9;//servo2
+int EXT_SERVO2 = 10;//servo1
+Servo servo_trigger;
+Servo servo_hose;
+
+int angle = 0;
+char currentCommand = 'z';
+bool swingHose = false;
 
 unsigned long lastCommandTime = 0;
-unsigned long commandTimeout = 1000; 
+unsigned long commandTimeout = 3000; // Changed to 3 seconds
+
+void resetExtinguisher()
+{
+    for (angle = 180; angle > 0; angle--)
+    {
+        servo_trigger.write(angle);
+        delay(15);
+    }
+    swingHose = false;
+}
 
 void reset()
 {
     resetExtinguisher();
-    lastCommandTime = 0;
+    lastCommandTime = millis(); // Resetting the timer when the reset function is called
     angle = 0;
     MOTOR_3.run(BACKWARD);
     MOTOR_4.run(BACKWARD);
 
     delay(5000);
-    
+
     MOTOR_3.run(RELEASE);
     MOTOR_4.run(RELEASE);
+
+    Serial.println("System reset.");
 }
 
 void triggerExtinguisher()
 {
-    for (angle = 0; angle < 180; angle++)
+    servo_trigger.write(0);
+    delay(300);
+    servo_trigger.write(180);
+    swingHose = true;
+}
+
+void executeCommand(char c)
+{
+    if (c == 'F')
     {
-        servo.write(angle);
-        delay(15);
+        MOTOR_3.run(FORWARD);
+        MOTOR_4.run(FORWARD);
     }
-}
-
-void resetExtinguisher()
-{
-    for(angle = 180; angle > 0; angle--) {
-        servo.write(angle);
-        delay(15);
+    else if(c == 'G'){
+        MOTOR_3.run(FORWARD);
+        MOTOR_4.run(FORWARD);
+        delay(30000);
     }
-}
-
-void receiveEvent(int bytes)
-{
-    lastCommandTime = millis(); 
-
-    while (Wire.available())
+    else if (c == 'B')
     {
-        char c = Wire.read();
-        if (c == 'F')
-        {
-            MOTOR_3.run(FORWARD);
-            MOTOR_4.run(FORWARD);
-        }
-        else if (c == 'B')
-        {
-            MOTOR_3.run(BACKWARD);
-            MOTOR_4.run(BACKWARD);
-        }
-        else if (c == 'L')
-        {
-            MOTOR_3.run(BACKWARD);
-            MOTOR_4.run(FORWARD);
-        }
-        else if (c == 'R')
-        {
-            MOTOR_3.run(FORWARD);
-            MOTOR_4.run(BACKWARD);
-        }
-        else if (c == 'S')
-        {
-            MOTOR_3.run(RELEASE);
-            MOTOR_4.run(RELEASE);
-        }
-        else if (c == 'E')
-        {
-            triggerExtinguisher();
-        }
-        else if (c == 'RST')
-        {
-            reset();
-        }
-        else
-        {
-            MOTOR_3.run(RELEASE);
-            MOTOR_4.run(RELEASE);
-        }
-        delay(1);
+        MOTOR_3.run(BACKWARD);
+        MOTOR_4.run(BACKWARD);
     }
-}
-
-void initMotors()
-{
-    MOTOR_3.setSpeed(100);
-    MOTOR_3.run(RELEASE);
-    MOTOR_4.setSpeed(100);
-    MOTOR_4.run(RELEASE);
-
-    servo.attach(EXT_SERVO);
-
-}
-
-void setup()
-{
-    Wire.begin(9);
-    Wire.onReceive(receiveEvent);
-
-    initMotors();
-}
-
-void loop() {
-
-    if (millis() - lastCommandTime > commandTimeout)
+    else if (c == 'L')
+    {
+        // MOTOR_3.run(BACKWARD);
+        MOTOR_4.run(FORWARD);
+    }
+    else if (c == 'R')
+    {
+        MOTOR_3.run(FORWARD);
+        // MOTOR_4.run(BACKWARD);
+    }
+    else if (c == 'S')
+    {
+        MOTOR_3.run(RELEASE);
+        MOTOR_4.run(RELEASE);
+    }
+    else if (c == 'E')
+    {
+        triggerExtinguisher();
+    }
+    else if (c == 'X')
+    {
+        reset();
+    }
+    else
     {
         MOTOR_3.run(RELEASE);
         MOTOR_4.run(RELEASE);
     }
 
+    Serial.print("Received command: ");
+    Serial.println(c);
+
+    // Update last command time
+    lastCommandTime = millis();
+}
+
+void receiveEvent(int bytes)
+{
+    while (Wire.available())
+    {
+        char c = Wire.read();
+        executeCommand(c);
+    }
+}
+
+void initMotors()
+{
+    MOTOR_3.setSpeed(255);
+    MOTOR_3.run(RELEASE);
+    MOTOR_4.setSpeed(255);
+    MOTOR_4.run(RELEASE);
+
+    servo_trigger.attach(EXT_SERVO1);
+    servo_hose.attach(EXT_SERVO2);
+}
+
+void initLEDs()
+{
+    LED_1.setSpeed(255);
+    LED_2.run(RELEASE);
+    LED_2.setSpeed(255);
+    LED_1.run(RELEASE);
+}
+
+void setup()
+{
+    pinMode(LED_BUILTIN,OUTPUT);
+    Serial.begin(9600);
+    Wire.begin(9);
+    Wire.onReceive(receiveEvent);
+
+    initMotors();
+    initLEDs();
+}
+
+void loop()
+{
+    // if(testRunTimer>timer){
+    //     delay(1);
+    //     timer++;
+    //     Serial.print(timer);
+    //     servo_hose.write(0);
+    //     servo_trigger.write(0);
+    //     MOTOR_3.run(FORWARD);
+    //     MOTOR_4.run(FORWARD);
+    //     delay(3000);
+    //     servo_hose.write(45);
+    //     servo_trigger.write(180);
+    //     MOTOR_3.run(RELEASE);
+    //     MOTOR_4.run(RELEASE);
+    //     delay(300);
+
+    //     return;
+    // }
+    if (swingHose)
+    {
+        servo_hose.write(0);
+        delay(300);
+        servo_hose.write(45);
+        delay(300);
+    }
 }
